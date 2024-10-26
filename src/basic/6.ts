6201542fc6434c6454f8e8f981246a07d43e08ff
Daria Havlovych <daria.havlovych@gmail.com>
2024-10-26 17:56:24 +0300

    ts first vertion

---
+++ src/basic/6.ts
@@ -0,0 +1,27 @@
+type userAddress = {
+  city: string;
+  country: string;
+};
+
+interface IUser {
+  name: string;
+  age: number;
+  email: string;
+  address?: userAddress;
+}
+
+const mango: IUser = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+const poly: IUser = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+};
